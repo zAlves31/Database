5586daae7a1250d4e5d41c9c867e1c4e2936c980
??? <>
2023-08-21 10:56:25 -0300

    21.08

---
+++ Lista_de_Exercicios/1.0_exercicio-filmes/DML - Listar Filmes.sql
@@ -0,0 +1,11 @@
+USE Filmes
+
+INSERT INTO Genero(Nome)
+VALUES ('Comedia'), ('Drama')
+
+INSERT INTO Filme(IdGenero,Titulo)
+VALUES (1, 'Gente Grande'), (2, 'O Pior Vizinho do Mundo')
+
+
+Select *from Genero
+Select *from Filme
